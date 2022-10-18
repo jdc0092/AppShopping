e96b86e3ddd0e3a476c53e15bc2cd280946125f0
jdc0092 <jherely1992@gmail.com>
2022-10-18 16:34:33 -0400

    Revert "Loading application in react native shopping"
    This reverts commit 964e31c824fb43666bad2b71249ab17d736791ff.

---
+++ Src/Css/styleDescriptions.js
@@ -0,0 +1,14 @@
+
+import { StyleSheet } from "react-native";
+
+
+const styleDescriptions = StyleSheet.create ({
+
+    productsDescriptions:{
+        textAlign: "center",
+        marginTop: 15
+    }
+    
+});
+
+export default styleDescriptions;
